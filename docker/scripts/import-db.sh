1bb248bc8f3f66fae779ea137924e03e73c6d270
Commeger <kdenock@gmail.com>
2026-07-27 15:00:41 +0200

    script for 3 dBs

--- docker/scripts/import-db.sh
+++ docker/scripts/import-db.sh
@@ -1,8 +1,9 @@
+
 #!/bin/bash
 mkdir -p DB_to_export
 if [ ! -f DB_to_export/openclinic_sync.sql ]; then
   echo "❌ DB_to_export/openclinic_sync.sql not found. Place it there before running this."
   exit 1
 fi
-docker exec -i openclinic-db mysql -uroot -proot openclinic < DB_to_export/openclinic_sync.sql
-echo "✅ Imported DB from DB_to_export/openclinic_sync.sql"
+docker exec -i openclinic-db mysql -uroot -proot < DB_to_export/openclinic_sync.sql
+echo "✅ Imported ocadmin_dbo, ocstats_dbo, openclinic_dbo from DB_to_export/openclinic_sync.sql"
